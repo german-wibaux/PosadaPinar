3eb83645e8af4725924919ff596660bf7cd44bdf
german <germanwibaux@gmail.com>
2018-07-07 16:39:50 -0300

    Arreglo get en server.js 1

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
     const index = path.join(__dirname, 'build', 'index.html');
     res.sendFile(index));
 });
