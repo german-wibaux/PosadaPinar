9489c88637d1b50b9a2668c7e5fc77264c238629
german <germanwibaux@gmail.com>
2018-07-07 16:36:53 -0300

    Arreglo get inserver.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index));
 });
 
 // Start the app by listening on the default Heroku port
