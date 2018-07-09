c897d9628e67ab639b9ef0252af3914c209b40e0
german <germanwibaux@gmail.com>
2018-07-08 22:07:51 -0300

    Nuevo deploy server.js

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
+// Run the app by serving the static files
+// in the dist directory
 app.use(express.static(__dirname + '/dist'));
-
-app.get('*', function(req,res) {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index));
-});
-
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
